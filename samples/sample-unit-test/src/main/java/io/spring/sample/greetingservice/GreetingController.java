7399449c10bf63469f249dcebe17f5cd629379fa
Patrick M.J. Roth <patrick.mj.roth@bluewin.ch>
2020-12-22 11:33:01 +0100

    Add sample for integration test Rename sample of unit test

---
+++ samples/sample-unit-test/src/main/java/io/spring/sample/greetingservice/GreetingController.java
@@ -0,0 +1,18 @@
+package io.spring.sample.greetingservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) throws ValidationException {
+            return Greeting.build(counter.incrementAndGet(), name);
+    }
+}
