85aa7e3ac69b35abdb19043f59afffa2606eeaa2
Patrick M.J. Roth <patrick.mj.roth@bluewin.ch>
2020-12-22 11:17:34 +0100

    Add sample for integration test Rename sample of unit test

--- samples/maven-service/src/main/java/io/spring/sample/greetingservice/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package io.spring.sample.greetingservice;
-
-public class ValidationException extends Exception {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
---
+++ samples/sample-integration-test/src/test/java/com/baeldung/quarkus/LibraryResourceHttpResourceIntegrationTest.java
@@ -0,0 +1,43 @@
+package com.baeldung.quarkus;
+
+import com.github.fuzzdbunit.params.provider.FuzzFile;
+import com.github.fuzzdbunit.params.provider.FuzzSource;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.common.http.TestHTTPResource;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.io.IOException;
+import java.net.URL;
+
+import static io.restassured.RestAssured.given;
+import static java.nio.charset.Charset.defaultCharset;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasItem;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@QuarkusTest
+class LibraryResourceHttpResourceIntegrationTest {
+
+    @TestHTTPEndpoint(LibraryResource.class)
+    @TestHTTPResource("book")
+    URL libraryEndpoint;
+
+    @ParameterizedTest
+    @FuzzSource(files = {FuzzFile.SQL_INJECTION_DETECT_GENERICBLIND})
+    void whenGetBooksByTitle_thenBookShouldBeFound(String name) {
+        System.out.println("Testing: "+name);
+        given().contentType(ContentType.JSON).param("query", name)
+                .when().get(libraryEndpoint)
+                .then().statusCode(200)
+                .body("size()", is(0));
+    }
+
+    @Test
+    void whenGetBooks_thenBooksShouldBeFound() throws IOException {
+        assertTrue(IOUtils.toString(libraryEndpoint.openStream(), defaultCharset()).contains("Asimov"));
+    }
+}
